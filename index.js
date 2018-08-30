94b11094c747b5c6ceab6e8edb5266f114451fec
BJR Matos <bjrmatos@gmail.com>
2018-08-30 12:14:41 -0500

    initial implementation

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const typeKeys = {
+  date: '$$$date$$$',
+  buffer: '$$$buffer$$$'
+}
+
+module.exports.serialize = (obj, { prettify = false, prettifySpace = 2 } = {}) => {
+  let res
+
+  const originalDateToJSON = Date.prototype.toJSON
+  const originalBufferToJSON = Buffer.prototype.toJSON
+
+  // Keep track of the fact that this is a Date object
+  Date.prototype.toJSON = function () { // eslint-disable-line
+    return { [typeKeys.date]: this.getTime() }
+  }
+
+  Buffer.prototype.toJSON = function (...args) { // eslint-disable-line
+    return { [typeKeys.buffer]: originalBufferToJSON.apply(this, args) }
+  }
+
+  res = JSON.stringify(obj, (key, value) => {
+    if (typeof value === 'undefined') {
+      return null
+    }
+
+    if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean' || value === null) {
+      return value
+    }
+
+    return value
+  }, prettify ? prettifySpace : null)
+
+  // Return Date to its original state
+  Date.prototype.toJSON = originalDateToJSON // eslint-disable-line
+  // Return Buffer to its original state
+  Buffer.prototype.toJSON = originalBufferToJSON // eslint-disable-line
+
+  return res
+}
+
+module.exports.parse = (json) => {
+  return JSON.parse(json, (key, value) => {
+    if (key === typeKeys.date) {
+      return new Date(value)
+    }
+
+    if (
+      key === typeKeys.buffer &&
+      value &&
+      value.type === 'Buffer'
+    ) {
+      return Buffer.from(value)
+    }
+
+    if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean' || value === null) {
+      return value
+    }
+
+    if (value && value[typeKeys.date]) {
+      return value[typeKeys.date]
+    }
+
+    if (value && value[typeKeys.buffer]) {
+      return value[typeKeys.buffer]
+    }
+
+    return value
+  })
+}
